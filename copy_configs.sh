d31f9603b870a7bb0b6a6ece151467549eb45462
nick <12878012+Tramscan@users.noreply.github.com>
2022-11-08 17:50:42 -0500

    made script to properly update config folders - will make script to properly update machine from repo later

---
+++ copy_configs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cp -r /etc/lightdm /home/nick/gh-configs/root/etc
+cp -r /etc/polybar /home/nick/gh-configs/root/etc
+
+cp -r /usr/share/fonts /home/nick/gh-configs/root/usr/share
+cp -r /usr/share/endeavouros /home/nick/gh-configs/root/usr/share
+cp -r /usr/share/rofi /home/nick/gh-configs/root/usr/share
+cp -r /usr/share/slick-greeter /home/nick/gh-configs/root/usr/share
+
+cp -r /home/nick/.config/ckb-next /home/nick/gh-configs/MONEY_HOME/.config
+cp -r /home/nick/.config/easyeffects /home/nick/gh-configs/MONEY_HOME/.config
+cp -r /home/nick/.config/i3 /home/nick/gh-configs/MONEY_HOME/.config
+cp -r /home/nick/.config/picom /home/nick/gh-configs/MONEY_HOME/.config
+cp -r /home/nick/.config/rofi /home/nick/gh-configs/MONEY_HOME/.config
+cp -r /home/nick/.config/xfce4 /home/nick/gh-configs/MONEY_HOME/.config
